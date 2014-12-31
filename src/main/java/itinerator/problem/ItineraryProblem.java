b28ccaa1ef5409b79c1c1d9ed1f12ffe9651e99d
Will Slotkin <william.slotkin@gmail.com>
2014-12-31 08:24:29 -0700

    fill in implementation for isSolution() method
    -configuration is valid if each event is within the start and end times and it doesn't start before the previous event ended

--- src/main/java/itinerator/problem/ItineraryProblem.java
+++ src/main/java/itinerator/problem/ItineraryProblem.java
@@ -5,6 +5,8 @@ import cz.cvut.felk.cig.jcop.util.JcopRandom;
 import itinerator.calculators.DistanceCalculator;
 import itinerator.calculators.TravelTimeCalculator;
 import itinerator.datamodel.Activity;
+import itinerator.datamodel.Event;
+import itinerator.datamodel.Itinerary;
 import itinerator.evaluator.ItineraryEvaluator;
 import itinerator.evaluator.ItineraryFactory;
 import itinerator.evaluator.ItineraryFitness;
@@ -18,22 +20,33 @@ public class ItineraryProblem extends BaseProblem implements GlobalSearchProblem
     private final List<Activity> activities;
     private final DateTime startTime;
     private final DateTime endTime;
+    private final ItineraryFactory itineraryFactory;
 
     public ItineraryProblem(List<Activity> activities, DateTime startTime, DateTime endTime) {
         this.activities = activities;
         this.startTime = startTime;
         this.endTime = endTime;
+        TravelTimeCalculator travelCalculator = new TravelTimeCalculator(new DistanceCalculator());
+        itineraryFactory = new ItineraryFactory(activities, startTime, endTime, travelCalculator);
     }
 
     @Override
     public boolean isSolution(Configuration configuration) {
+        Itinerary itinerary = itineraryFactory.create(configuration);
+        DateTime endTimeOfPrevious = startTime;
+        for (Event event : itinerary.getEvents()) {
+            DateTime eventStart = event.getEventTime().getStart();
+            DateTime eventEnd = event.getEventTime().getEnd();
+            if (endTimeOfPrevious.isAfter(eventStart) || eventStart.isBefore(startTime) || eventEnd.isAfter(endTime)) {
+                return false;
+            }
+            endTimeOfPrevious = eventEnd;
+        }
         return true;
     }
 
     @Override
     public Fitness getDefaultFitness() {
-        TravelTimeCalculator travelCalculator = new TravelTimeCalculator(new DistanceCalculator());
-        ItineraryFactory itineraryFactory = new ItineraryFactory(activities, startTime, endTime, travelCalculator);
         return new ItineraryFitness(activities, new ItineraryEvaluator(), itineraryFactory);
     }
 
