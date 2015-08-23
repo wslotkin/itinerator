a7c73c722b9448907f78f268834720504b074e46
Will Slotkin <william.slotkin@gmail.com>
2015-08-23 13:01:48 -0500

    Use score column if available
    - Attempt to use score value directly and fall back to average rating
     if missing

--- generator/src/main/java/itinerator/data/TextDataLoader.java
+++ generator/src/main/java/itinerator/data/TextDataLoader.java
@@ -12,14 +12,15 @@ import static java.lang.Integer.parseInt;
 import static java.lang.Long.parseLong;
 
 class TextDataLoader extends AbstractDataLoader {
-
     private static final int ID_COLUMN = 2;
     private static final int DURATION_COLUMN = 4;
     private static final int LONGITUDE_COLUMN = 5;
     private static final int LATITUDE_COLUMN = 6;
-    private static final int SCORE_COLUMN = 13;
+    private static final int SCORE_COLUMN = 6;
+    private static final int AVERAGE_RATING_COLUMN = 13;
     private static final int HOURS_COLUMN = 14;
     private static final int TYPE_COLUMN = 17;
+    private static final String MISSING_VALUE_INDICATOR = "None";
 
     public TextDataLoader() {
         super(FileType.TEXT);
@@ -27,17 +28,16 @@ class TextDataLoader extends AbstractDataLoader {
 
     @Override
     protected Activity parseRowElements(String[] rowElements) {
+        WeeklySchedule weeklySchedule = parseHours(rowElements[HOURS_COLUMN]);
+        double score = getScore(rowElements);
         Location location = new Location(optionalDouble(rowElements[LATITUDE_COLUMN]),
                 optionalDouble(rowElements[LONGITUDE_COLUMN]));
 
-        String hoursString = rowElements[HOURS_COLUMN];
-        WeeklySchedule weeklySchedule = parseHours(hoursString);
-
         ActivityBuilder activityBuilder = new ActivityBuilder()
                 .setId(rowElements[ID_COLUMN])
-                .setLocation(location)
-                .setScore(optionalDouble(rowElements[SCORE_COLUMN]))
                 .setWeeklySchedule(weeklySchedule)
+                .setScore(score)
+                .setLocation(location)
                 .setType(ActivityType.valueOf(rowElements[TYPE_COLUMN].toUpperCase()));
 
         String durationString = rowElements[DURATION_COLUMN];
@@ -48,9 +48,9 @@ class TextDataLoader extends AbstractDataLoader {
         return activityBuilder.build();
     }
 
-    private static double optionalDouble(String rowElement) {
+    private static double optionalDouble(String stringValue) {
         try {
-            return parseDouble(rowElement);
+            return parseDouble(stringValue);
         } catch (NumberFormatException exception) {
             return 0.0;
         }
@@ -70,6 +70,16 @@ class TextDataLoader extends AbstractDataLoader {
         }
 
         return new WeeklySchedule(weeklyShifts);
+    }
+
+    private static double getScore(String[] rowElements) {
+        String scoreString = rowElements[SCORE_COLUMN];
+
+        if (!MISSING_VALUE_INDICATOR.equals(scoreString)) {
+            return optionalDouble(scoreString);
+        } else {
+            return optionalDouble(rowElements[AVERAGE_RATING_COLUMN]);
+        }
     }
 
     private static WeeklyShift parseShift(String shift) {
