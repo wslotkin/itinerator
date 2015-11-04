a7e23318ecace3f1b9fea78522be35e87bf984ab
Will Slotkin <william.slotkin@gmail.com>
2015-11-03 21:11:08 -0600

    Add performance test around optimizer

---
+++ performance/src/main/java/itinerator/OptimizerBenchmark.java
@@ -0,0 +1,22 @@
+package itinerator;
+
+import itinerator.config.GeneticAlgorithmConfig;
+import itinerator.config.OptimizationConfig;
+import itinerator.main.OptimizerMain;
+import itinerator.solver.ItinerarySolver;
+import org.openjdk.jmh.annotations.Benchmark;
+
+import java.io.IOException;
+
+public class OptimizerBenchmark {
+
+    @Benchmark
+    public ItinerarySolver.SolverResult runOptimizer() throws IOException {
+        return new OptimizerMain(new OptimizationConfig.Builder()
+                .setGeneticAlgorithmConfig(new GeneticAlgorithmConfig.Builder()
+                        .setPopulationSize(100)
+                        .setMaxIterations(50)
+                        .build())
+                .build()).run();
+    }
+}
