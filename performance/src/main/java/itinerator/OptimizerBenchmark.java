19a441c33f9b327d54832853a9a3a4adc44da52f
Will Slotkin <william.slotkin@gmail.com>
2015-11-04 23:37:54 -0600

    Add file to log benchmarks

--- performance/src/main/java/itinerator/OptimizerBenchmark.java
+++ performance/src/main/java/itinerator/OptimizerBenchmark.java
@@ -10,11 +10,7 @@ import java.io.IOException;
 
 public class OptimizerBenchmark {
 
-//  11/3/15, commit a7e2331
-//  Result "runOptimizer":
-//  4.849 ±(99.9%) 0.154 ops/s [Average]
-//  (min, avg, max) = (1.984, 4.849, 6.041), stdev = 0.653
-//  CI (99.9%): [4.695, 5.003] (assumes normal distribution)
+    // See benchmarkResults.txt
     @Benchmark
     public ItinerarySolver.SolverResult runOptimizer() throws IOException {
         return new OptimizerMain(new OptimizationConfig.Builder()
