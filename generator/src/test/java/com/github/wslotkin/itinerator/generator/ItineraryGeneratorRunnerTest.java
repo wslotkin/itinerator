0c569fbb4fd5e8052ff26ce6dd29a95e47b029cb
Will Slotkin <william.slotkin@gmail.com>
2016-12-11 18:15:12 -0600

    Use smaller optimization in unit test

--- generator/src/test/java/com/github/wslotkin/itinerator/generator/ItineraryGeneratorRunnerTest.java
+++ generator/src/test/java/com/github/wslotkin/itinerator/generator/ItineraryGeneratorRunnerTest.java
@@ -1,5 +1,6 @@
 package com.github.wslotkin.itinerator.generator;
 
+import com.github.wslotkin.itinerator.generator.config.ImmutableGeneticAlgorithmConfig;
 import com.github.wslotkin.itinerator.generator.config.ImmutableOptimizationConfig;
 import com.github.wslotkin.itinerator.generator.data.DataFileActivityProvider;
 import com.github.wslotkin.itinerator.generator.datamodel.SolverResult;
@@ -12,7 +13,12 @@ import static org.junit.Assert.assertNotNull;
 public class ItineraryGeneratorRunnerTest {
     @Test
     public void canRunOptimization() {
-        ItineraryGeneratorRunner optimizer = createOptimizer(ImmutableOptimizationConfig.builder().build(),
+        ItineraryGeneratorRunner optimizer = createOptimizer(ImmutableOptimizationConfig.builder()
+                        .geneticAlgorithmConfig(ImmutableGeneticAlgorithmConfig.builder()
+                                .maxIterations(10)
+                                .populationSize(20)
+                                .build())
+                        .build(),
                 new DataFileActivityProvider(BEIJING_DATA));
         SolverResult optimizerResult = optimizer.run();
         assertNotNull(optimizerResult);
